8f3754e1c9e43bd69d4f52c9aee9eae7f3483dd4
pmolina4 <pablomolinaconde4@gmail.com>
2022-12-09 19:16:35 +0100

    simulacro postal navidad

---
+++ Simulacros/simulacro_examen_navidad/funcion.js
@@ -0,0 +1,17 @@
+
+//MOSTRAR SALUDO CUANDO PASAN 5sg DEL INICIO DE LA PÁGINA
+const myTimeout = setTimeout(mostrarSaludo, 3000);
+
+function mostrarSaludo() {
+  document.getElementById("saludos").style.visibility="visible";
+}
+
+
+
+//OCULTAR SALUDO AL HACER CLICK EN EL BODY
+let body = document.querySelector("body");
+body.addEventListener("click", ocultarSaludo);
+
+function ocultarSaludo() {
+    document.getElementById("saludos").style.visibility="hidden";
+  }
