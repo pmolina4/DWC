2359bc86c70104c405ef2ecde4ebb1dce991caa6
pmolina4 <pablomolinaconde4@gmail.com>
2022-12-14 00:05:04 +0100

    Postal navidena

--- Simulacros/postal/funcion.js
+++ Simulacros/postal/funcion.js
@@ -1,10 +1,14 @@
+var audio = document.getElementById("audio");
+var audioPuerta = document.getElementById("audioPuerta");
+var audioNavidad = document.getElementById("cancionNavidad");
+
 //MOSTRAR SALUDO CUANDO PASAN 5sg DEL INICIO DE LA PÁGINA
 const myTimeout = setTimeout(mostrarSaludo, 3000);
 
 function mostrarSaludo() {
-  document.getElementById("saludos").style.visibility="visible";
+  document.getElementById("saludos").style.visibility = "visible";
+  document.getElementById("grinch").style.visibility = "visible";
 }
-
 
 
 //OCULTAR SALUDO AL HACER CLICK EN EL BODY
@@ -12,6 +16,39 @@ let body = document.querySelector("body");
 body.addEventListener("click", ocultarSaludo);
 
 function ocultarSaludo() {
-    document.getElementById("saludos").style.visibility="visible";
+  document.getElementById("saludos").style.visibility = "hidden";
+}
+
+//Mover la posicion del grinch y poner la puerta visible con la musica
+document.getElementById("grinch").addEventListener("click", function () {
+  document.getElementById("grinch").style.transform = "scaleX(-1)";
+  document.getElementById("puerta").style.visibility = "visible";
+  audio.play();
+});
+
+
+//Abrir Puerta con muscia y para la musica de el grinch y borrar el grinch
+document.getElementById("puerta").addEventListener("click", function () {
+  document.getElementById("grinch").style.visibility = "hidden";
+  audio.pause();
+  audioPuerta.play();
+
+  const tiempoCierre = setTimeout(borrarPuerta, 3000);
+
+  function borrarPuerta() {
+    document.getElementById("puerta").style.visibility = "hidden";
+    document.getElementById("felizGrinch").style.visibility = "visible";
+    audioPuerta.pause();
+    audioNavidad.play();
   }
+
+
+
+});
+
+
+
+
+
+
 
