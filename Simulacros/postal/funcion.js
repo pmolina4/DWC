982cc6bf740ae92fdec69c80fd434823f7f59a9c
pmolina4 <pablomolinaconde4@gmail.com>
2022-12-14 11:56:24 +0100

    postal

--- Simulacros/postal/funcion.js
+++ Simulacros/postal/funcion.js
@@ -37,7 +37,7 @@ document.getElementById("puerta").addEventListener("click", function () {
 
   setTimeout(borrarPuerta, 3000); // ejecutamos la funcuon borrar puerta a los 3seg
 
-  //esta funcion lo que hace es ocultar la puerta y mostrar el logo de el grinch con la cancion de navidad
+//Esta funcion lo que hace es ocultar la puerta y mostrar el logo de el grinch con la cancion de navidad
   function borrarPuerta() {
     document.getElementById("puerta").style.visibility = "hidden";
     document.getElementById("felizGrinch").style.visibility = "visible";
@@ -47,27 +47,25 @@ document.getElementById("puerta").addEventListener("click", function () {
 });
 
 
-window.addEventListener("orientationchange", () => {
-  if (window.matchMedia("(orientation: landscape)").matches) {
-    document.getElementById("grinch").style.visibility = "hidden";  //Ocultamos el grinch
-    document.getElementById("puerta").style.visibility = "hidden"; //ocultamos la puerta
-    document.getElementById("felizGrinch").style.visibility = "visible";
-    document.getElementById("felizGrinch").style.width = "30%";
-    document.getElementById("felizGrinch").style.height = "30%";
-    document.getElementById("felizGrinch").style.marginLeft = "30%";
-    document.getElementById("felizGrinch").style.marginTop = "74%";
-    audio.pause();
-    audioPuerta.pause();
-    audioNavidad.pause();
-    musica.play();
+//DETECTAR LA ORIENTACION DE LA PANTALLA CON JAVASCRIPT
+window.addEventListener("orientationchange", ()=> {
+  //CUANDO ESTA EN PORTRAIT HACEMOS LO QUE HAY DENTRO DEL CONDICIONAL
+  if (window.screen.orientation.type == "portrait-primary") {
+    //RECARGAMOS LA PÁGINA CUANDO GIREMOS HA PORTRAIR
+    location.reload();
   }
- 
+
+    //CUANDO ESTA EN LANDSCAPE HACEMOS LO QUE HAY DENTRO DEL CONDICIONAL
+  if (window.screen.orientation.type == "landscape-primary") {
+      document.getElementById("grinch").style.visibility = "hidden";//ocultamos el grinch
+      document.getElementById("saludos").style.visibility = "hidden"; //ocultamos el saludo
+      document.getElementById("felizGrinch").style.visibility = "visible"; //mostramos el grinc de felicidades
+      document.getElementById("felizGrinch").style.width = "32vw"; // le cambiamos el tamaño
+      //Se para toda la musica 
+      audio.pause();
+      audioPuerta.pause();
+      audioNavidad.pause();
+      musica.play();//se repodruce la nueva musica
+  }
+
 });
-
-
-
-
-
-
-
-
